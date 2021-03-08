1d12e3ff2eaac01c0d2dccf2ef3925f8a31cf1eb
John Owuor <owuor91@gmail.com>
2021-03-08 09:16:17 +0300

    first commit

---
+++ src/main/java/dev/owuor91/rabbitcalc/RabbitCalcApplication.java
@@ -0,0 +1,13 @@
+package dev.owuor91.rabbitcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/owuor91/rabbitcalc/RabbitCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.owuor91.rabbitcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
